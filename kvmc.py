e7b269327e38db2fe6f4d5ea91eb028946325d9e
Alexandre Kandalintsev <spam@messir.net>
2013-03-19 18:23:06 +0100

    renamed:    newconfig.py -> kvmc.py

--- kvmc.py
+++ kvmc.py
@@ -1,142 +1,176 @@
 #!/usr/bin/env python3
-import exeutilz; exeutilz.exeutilz_minver(1.75)
 from exeutilz.cli import CLI, command
 from useful.tmux import TMUX
-from useful.small import run
 from useful.log import Log
-import configparser
-import subprocess
+
+from collections import OrderedDict
+from functools import reduce
 import argparse
 import socket
 import signal
-import shlex
 import errno
 import time
 import sys
 import os
 
-__version__ = 5
-
-DEBUG = True
+__version__ = 6
 CONFIGS = list(map(os.path.expanduser, ['~/kvmc.cfg', '/etc/kvmc.cfg']))
 BUF_SIZE = 65535
-log = Log("KVMC", verbosity='info')
+log = Log("KVMC")
+
+#TODO: why reduce doesn't work?
+def stringify(iterable):
+  # return reduce(lambda x, y: str(x)+str(y)+" ", iterable)
+  r = ""
+  for it in iterable:
+    r += str(it)
+  return r+" "
 
 
-class StatusUnknown(Exception):
-    pass
+kvms = OrderedDict()
+class MetaKVM(type):
+  def __init__(cls, name, bases, ns):
+    global kvms
+    if not ns.get('template', False):
+      assert name.lower() not in kvms, "duplicate name: %s" % name
+      kvms[name.lower()] = cls()
 
 
-def validate_name(name):
-    if name != name.strip():
-        raise Exception("Name ``%s'' contains trailing"
-                        "or leading whitespace characters" % name)
+class KVM(metaclass=MetaKVM):
+  name = None
+  mem  = 256
+  cores = 1
+  cpu = "qemu64"
+  runas = None
+  tmux = TMUX(session="virt")
+  template = True
 
-    if name in ["status", "kill", "start", "stop", "all", "shutdown"]:
-        raise Exception("Invalid name for the machine: %s" % name)
+  def __init__(self):
+    self.name = self.name or self.__class__.__name__
+    self.pidfile = "/var/tmp/kvm_%s.pid" % self.name
+    self.monfile = "/var/tmp/kvm_%s.mon" % self.name
+    self.log = Log("KVM %s" % self.name)
 
-def fatal_error(error, errno=1):
-    log.error(error)
-    sys.exit(errno)
+  def get_cmd(self):
+    cmd = self.cmd
+    cmd += " -name %s" % self.name
+    cmd += " -m %s" % self.mem
+    cmd += " -cpu %s" % self.cpu
+    cmd += " -smp %s" % self.cores
+    cmd += " -monitor unix:%s,server,nowait " % self.monfile
+    cmd += " -pidfile %s " % self.pidfile
+    cmd += stringify(self.net)
+    cmd += stringify(self.drives)
+    if self.runas:
+      if os.geteuid() != 0:
+        cmd = "sudo " + cmd
+      cmd += " -runas %s" % self.runas
+    return cmd
 
-
-
-class KVM(CLI):
-    def __init__(self, name=None, mem=384, cores=1, sleep=0.0, cpu=None, runas=None, cmd="", tmux=None, qemu="qemu-system-x86_64 --enable-kvm"):
-      self.name = name
-      self.tmux = tmux
-      self.sleep = float(sleep)
-      self.pidfile = "/var/tmp/kvm_%s.pid" % self.name
-      self.cmd = qemu + " -curses -pidfile %s " % self.pidfile
-      self.monfile = "/var/tmp/kvm_%s.mon" % self.name
-      self.cmd += "-monitor unix:%s,server,nowait " % self.monfile
-      self.cmd += "-name %s " % self.name
-      if mem:    self.cmd += "-m %s " % mem
-      if cores:  self.cmd += "-smp %s " % cores
-      if cpu:    self.cmd += "-cpu %s " % cpu
-      if cmd:    self.cmd += cmd
-
-      if runas:
-        self.cmd = "sudo " + self.cmd # "-runas" requires root privs #TODO: check for root?
-        self.cmd += " -runas %s " % runas
-
-    def is_running(self):
-        try:
-          pid = int(open(self.pidfile).readline().strip())
-        except IOError as err:
-          if err.errno == errno.EACCES:
-            raise StatusUnknown("cannot read pidfile:", err)
-          elif err.errno == errno.ENOENT:
-            return False
-          raise
-
-        try:
-          os.kill(pid, 0)
-          return pid
-        except ProcessLookupError:
-          os.unlink(self.pidfile)
+  def is_running(self):
+      try:
+        pid = int(open(self.pidfile).readline().strip())
+      except IOError as err:
+        if err.errno == errno.EACCES:
+          raise StatusUnknown("cannot read pidfile:", err)
+        elif err.errno == errno.ENOENT:
           return False
+        raise
 
-    def start(self):
-      if self.is_running():
-        print("Instance is already started!")
+      try:
+        os.kill(pid, 0)
+        return pid
+      except ProcessLookupError:
+        os.unlink(self.pidfile)
         return False
 
-      print("spawning", self.cmd)
-      self.tmux.run(self.cmd, name=self.name)
+  def start(self):
+    if self.is_running():
+      self.log.error("Instance is already started!")
+      return False
 
-    #TODO: check its uniqueness
-    def gen_mac(self):
-      #from http://mediakey.dk/~cc/generate-random-mac-address-for-e-g-xen-guests/
-      mac = [ 0x52, 0x54, 0x00,
-      random.randint(0x00, 0xff),
-      random.randint(0x00, 0xff),
-      random.randint(0x00, 0xff) ]
-      return ':'.join(map(lambda x: "%02x" % x, mac))
+    self.log.debug("spawning %s" % self.get_cmd())
+    self.tmux.run(self.get_cmd(), name=self.name)
 
-    def kill(self):
+  #TODO: check its uniqueness
+  def gen_mac(self):
+    #from http://mediakey.dk/~cc/generate-random-mac-address-for-e-g-xen-guests/
+    mac = [ 0x52, 0x54, 0x00,
+    random.randint(0x00, 0xff),
+    random.randint(0x00, 0xff),
+    random.randint(0x00, 0xff) ]
+    return ':'.join(map(lambda x: "%02x" % x, mac))
+
+  def kill(self):
+    pid = self.is_running()
+    if not pid:
+      return
+    os.kill(pid, signal.SIGTERM)
+
+  def send_cmd(self, cmd):
+    if isinstance(cmd, str):
+      cmd = cmd.encode()
+    if not cmd.endswith(b'\n'):
+      cmd += b'\n'
+
+    s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+    s.settimeout(1)
+    self.log.debug("connecting to %s" % self.monfile)
+    s.connect(self.monfile)
+    s.send(cmd)
+    answer = s.recv(BUF_SIZE)
+    if len(answer) == BUF_SIZE:
+      self.log.error("too long answer was truncated :(")
+    self.log.notice(answer.decode(errors='replace'))
+    return answer
+
+  def console(self):
+    self.tmux.attach(name=self.name)
+
+  def reboot(self):
+    self.send_cmd("sendkey ctrl-alt-delete")
+
+  def shutdown(self):
+    self.send_cmd("system_powerdown")
+
+  def reset(self):
+    self.send_cmd("system_reset")
+
+  def print_status(self):
+    print("{name}".format(name=self.name))
+    try:
       pid = self.is_running()
-      if not pid:
-        return
-      os.kill(pid, signal.SIGTERM)
+      status = "UP (pid %s)" % pid if pid else "DOWN"
+    except StatusUnknown as err:
+      status = "UNKNOWN (%s)" % err
+    print ("  status:", status)
 
-    def send_cmd(self, cmd):
-      if isinstance(cmd, str):
-        cmd = cmd.encode()
-      if not cmd.endswith(b'\n'):
-        cmd += b'\n'
+  def __repr__(self):
+    return "<KVM(name=\"{name}\")>".format(name=self.name)
 
-      s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-      s.settimeout(1)
-      log.debug("connecting to %s" % self.monfile)
-      s.connect(self.monfile)
-      s.send(cmd)
-      answer = s.recv(BUF_SIZE)
-      if len(answer) == BUF_SIZE:
-        log.error("too long answer was truncated :(")
-      log.notice(answer.decode(errors='replace'))
-      return answer
 
-    def reboot(self):
-      self.send_cmd("sendkey ctrl-alt-delete")
+class Bridged:
+  def __init__(self, nic, model, mac, br):
+    self.nic   = nic
+    self.model = model
+    self.mac   = mac  # TODO: validate MAC
+    self.br    = br
 
-    def shutdown(self):
-      self.send_cmd("system_powerdown")
+  def __str__(self):
+    cmd = "-net nic,model={model},macaddr={mac} -net bridge,br={br}" \
+           .format(model=self.model, br=self.br, mac=self.mac)
+    return cmd
 
-    def reset(self):
-      self.send_cmd("system_reset")
 
-    def print_status(self):
-      print("{name}".format(name=self.name))
-      try:
-        pid = self.is_running()
-        status = "UP (pid %s)" % pid if pid else "DOWN"
-      except StatusUnknown as err:
-        status = "UNKNOWN (%s)" % err
-      print ("  status:", status)
+class Drive:
+  def __init__(self, path, cache="writeback"):
+    self.path = path
+    self.cache = cache
 
-    def __repr__(self):
-      return "<KVM(name=\"{name}\")>".format(name=self.name)
+  def __str__(self):
+    cmd = "-drive file={path},cache={cache}" \
+          .format(path=self.path, cache=self.cache)
+    return cmd
 
 
 class CMD(CLI):
@@ -162,10 +196,10 @@ class CMD(CLI):
         sleep = 0
         for instance in self.instances.values():
             if sleep:
-                log.debug("sleeping for %s before launchin another instance" % sleep)
+                log.debug("sleeping for %s before launching another instance" % sleep)
             time.sleep(sleep)
             if instance.is_running():
-                log.debug("skipping %s because it is allready started" % instance)
+                log.debug("skipping %s because it is already started" % instance)
                 continue
             log.info("Starting %s" % instance)
             instance.start()
@@ -210,20 +244,8 @@ class CMD(CLI):
     def do_reset(self, name=None):
         instances[name].reset()
 
-    @command("version")
-    def do_print_version(self):
-        print("version: ", __version__)
-        print(sys.version)
 
-    def do_default(self):
-        return self.do_status()
-
-
-if __name__ == '__main__':
-  instances = {}
-  os.unsetenv('TMUX')
-  tmux = TMUX()
-
+def main():
   parser = argparse.ArgumentParser(
     description='KVM commander version %s' % __version__)
   parser.add_argument('-d', '--debug', action='store_true',
@@ -234,11 +256,10 @@ if __name__ == '__main__':
   parser.add_argument('cmd', default="status", nargs="*",
     help="command to execute")
   args = parser.parse_args()
-  print(args)
+  print("arguments:", args, file=sys.stderr)
 
   if args.version:
-    print("Software version:", __version__)
-    sys.exit()
+    return print("Software version:", __version__)
 
   if args.debug:
     log.verbosity = "debug"
@@ -246,19 +267,5 @@ if __name__ == '__main__':
   if args.config:
     CONFIGS = [args.config]
 
-  config = configparser.ConfigParser()
-  for cfgfile in CONFIGS:
-    r = config.read(cfgfile)
-    if r:
-      log.debug("Using config file %s" % cfgfile)
-      break
-  else:
-    raise Exception("Cannot read any of config files (%s)" % CONFIGS)
-
-  for name in config.sections():
-    params = config[name]
-    instance = KVM(tmux=tmux, name=name, **params)
-    instances[name] = instance
-
-  cmd = CMD(instances)
+  cmd = CMD(kvms)
   cmd.run_cmd(args.cmd)
