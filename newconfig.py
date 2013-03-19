d42218cc397d240d0084179dc49532277ba31bf4
Alexandre Kandalintsev <spam@messir.net>
2013-03-19 18:17:28 +0100

    first half-working version

--- newconfig.py
+++ newconfig.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python3
-from functools import reduce
+from exeutilz.cli import CLI, command
 from useful.tmux import TMUX
 from useful.log import Log
+
+from collections import OrderedDict
+from functools import reduce
+import argparse
 import signal
 import errno
+import sys
 import os
+
+__version__ = 6
+CONFIGS = list(map(os.path.expanduser, ['~/kvmc.cfg', '/etc/kvmc.cfg']))
+BUF_SIZE = 65535
+log = Log("KVMC")
 
 #TODO: why reduce doesn't work?
 def stringify(iterable):
@@ -15,13 +25,23 @@ def stringify(iterable):
   return r+" "
 
 
-class KVM:
+kvms = OrderedDict()
+class MetaKVM(type):
+  def __init__(cls, name, bases, ns):
+    global kvms
+    if not ns.get('template', False):
+      assert name.lower() not in kvms, "duplicate name: %s" % name
+      kvms[name.lower()] = cls()
+
+
+class KVM(metaclass=MetaKVM):
   name = None
   mem  = 256
   cores = 1
   cpu = "qemu64"
   runas = None
   tmux = TMUX(session="virt")
+  template = True
 
   def __init__(self):
     self.name = self.name or self.__class__.__name__
@@ -29,7 +49,7 @@ class KVM:
     self.monfile = "/var/tmp/kvm_%s.mon" % self.name
     self.log = Log("KVM %s" % self.name)
 
-  def __str__(self):
+  def get_cmd(self):
     cmd = self.cmd
     cmd += " -name %s" % self.name
     cmd += " -m %s" % self.mem
@@ -67,8 +87,8 @@ class KVM:
       self.log.error("Instance is already started!")
       return False
 
-    self.log.debug("spawning %s" % self)
-    self.tmux.run(self, name=self.name)
+    self.log.debug("spawning %s" % self.get_cmd())
+    self.tmux.run(self.get_cmd(), name=self.name)
 
   #TODO: check its uniqueness
   def gen_mac(self):
@@ -93,14 +113,17 @@ class KVM:
 
     s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
     s.settimeout(1)
-    log.debug("connecting to %s" % self.monfile)
+    self.log.debug("connecting to %s" % self.monfile)
     s.connect(self.monfile)
     s.send(cmd)
     answer = s.recv(BUF_SIZE)
     if len(answer) == BUF_SIZE:
-      log.error("too long answer was truncated :(")
-    log.notice(answer.decode(errors='replace'))
+      self.log.error("too long answer was truncated :(")
+    self.log.notice(answer.decode(errors='replace'))
     return answer
+
+  def console(self):
+    self.tmux.attach(name=self.name)
 
   def reboot(self):
     self.send_cmd("sendkey ctrl-alt-delete")
@@ -148,6 +171,78 @@ class Drive:
     return cmd
 
 
+class CMD(CLI):
+    def __init__(self, instances):
+        self.instances = instances
+
+    @command("list")
+    def do_list(self):
+        for kvm in self.instances:
+            print(kvm)
+
+    @command("[name] start")
+    @command("start [name]")
+    def do_start(self, name=None):
+        if name not in self.instances:
+            fatal_error("no such instance: %s" % name)
+        print("Starting %s" % name)
+        self.instances[name].start()
+
+    @command("start all")
+    def do_start_all(self):
+        log.debug("starting all not started instances")
+        sleep = 0
+        for instance in self.instances.values():
+            if sleep:
+                log.debug("sleeping for %s before launching another instance" % sleep)
+            time.sleep(sleep)
+            if instance.is_running():
+                log.debug("skipping %s because it is already started" % instance)
+                continue
+            log.info("Starting %s" % instance)
+            instance.start()
+            sleep = instance.sleep
+
+    @command("console")
+    @command("console [name]")
+    @command("[name] console")
+    def do_console(self, name=None):
+        print("attaching", name)
+        if name and not instances[name].is_running():
+                sys.exit("Instance is not started")
+        tmux.attach(name=name)
+
+    @command("status")
+    def do_status(self):
+        for instance in self.instances.values():
+            instance.print_status()
+
+    @command("kill all") #TODO: doesnt work
+    def do_kill_all(self):
+        for instance in self.instances.values():
+            instance.kill()
+
+    @command("[name] shutdown")
+    @command("shutdown [name]")
+    def do_shutdown(self, name=None):
+        self.instances[name].shutdown()
+
+    @command("[name] kill")
+    @command("kill [name]")
+    def do_kill(self, name=None):
+        instances[name].kill()
+
+    @command("[name] reboot")
+    @command("reboot [name]")
+    def do_reboot(self, name=None):
+        instances[name].reboot()
+
+    @command("[name] reset")
+    @command("reset [name]")
+    def do_reset(self, name=None):
+        instances[name].reset()
+
+
 class Default(KVM):
   mem   = 384
   cpu   = "phenom"
@@ -156,6 +251,7 @@ class Default(KVM):
   auto  = True
   drives= []
   cmd   = "qemu-system-x86_64 --enable-kvm -curses"
+  template = True
 
 
 class LOR(Default):
@@ -164,6 +260,30 @@ class LOR(Default):
   drives = [Drive("/home/exe/lor.qcow2", cache="unsafe")]
 
 
-# b = Bridged(nic="lor", model='virtio', mac="blah-blah", br="extbr")
-kvm = LOR()
-kvm.start()
+def main():
+  parser = argparse.ArgumentParser(
+    description='KVM commander version %s' % __version__)
+  parser.add_argument('-d', '--debug', action='store_true',
+                      default=False, help="enable debug output")
+  parser.add_argument('-c', '--config', help="path to config file")
+  parser.add_argument('-v', '-V', '--version', action='store_true',
+                      default=False, help="get software version")
+  parser.add_argument('cmd', default="status", nargs="*",
+    help="command to execute")
+  args = parser.parse_args()
+  print("arguments:", args, file=sys.stderr)
+
+  if args.version:
+    return print("Software version:", __version__)
+
+  if args.debug:
+    log.verbosity = "debug"
+
+  if args.config:
+    CONFIGS = [args.config]
+
+  cmd = CMD(kvms)
+  cmd.run_cmd(args.cmd)
+
+if __name__ == '__main__':
+  main()
