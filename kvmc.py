834387c95f510308785ca9a9f80c4f4c8dc030c6
Alexandre Kandalintsev <spam@messir.net>
2013-03-19 14:04:46 +0100

    just formatting fixes and trivial refactoring

--- kvmc.py
+++ kvmc.py
@@ -22,17 +22,17 @@ BUF_SIZE = 65535
 log = Log("KVMC", verbosity='info')
 
 
-def validate_name(name):
-    if name != name.strip():
-        raise Exception("Name ``%s'' contains trailing or leading whitespace characters" % name)
-
-    if name in ["status", "kill", "start", "stop", "all", "shutdown"]:
-        raise Exception("Invalid name for the machine: %s" % name)
-
-
 class StatusUnknown(Exception):
     pass
 
+
+def validate_name(name):
+    if name != name.strip():
+        raise Exception("Name ``%s'' contains trailing"
+                        "or leading whitespace characters" % name)
+
+    if name in ["status", "kill", "start", "stop", "all", "shutdown"]:
+        raise Exception("Invalid name for the machine: %s" % name)
 
 def fatal_error(error, errno=1):
     log.error(error)
@@ -43,122 +43,110 @@ def run(cmd):
     subprocess.check_call(cmd)
 
 
-
 class KVM(CLI):
     def __init__(self, name=None, mem=384, cores=1, sleep=0.0, cpu=None, runas=None, cmd="", tmux=None, qemu="qemu-system-x86_64 --enable-kvm"):
-        self.name = name
-        self.tmux = tmux
-        self.sleep = float(sleep)
-        self.pidfile = "/var/tmp/kvm_%s.pid" % self.name
-        self.cmd = qemu + " -curses -pidfile %s " % self.pidfile
-        self.monfile = "/var/tmp/kvm_%s.mon" % self.name
-        self.cmd += "-monitor unix:%s,server,nowait " % self.monfile
-        self.cmd += "-name %s " % self.name
-        if mem:    self.cmd += "-m %s " % mem
-        if cores:  self.cmd += "-smp %s " % cores
-        if cpu:    self.cmd += "-cpu %s " % cpu
-        if cmd:    self.cmd += cmd
+      self.name = name
+      self.tmux = tmux
+      self.sleep = float(sleep)
+      self.pidfile = "/var/tmp/kvm_%s.pid" % self.name
+      self.cmd = qemu + " -curses -pidfile %s " % self.pidfile
+      self.monfile = "/var/tmp/kvm_%s.mon" % self.name
+      self.cmd += "-monitor unix:%s,server,nowait " % self.monfile
+      self.cmd += "-name %s " % self.name
+      if mem:    self.cmd += "-m %s " % mem
+      if cores:  self.cmd += "-smp %s " % cores
+      if cpu:    self.cmd += "-cpu %s " % cpu
+      if cmd:    self.cmd += cmd
 
-        if runas:
-            self.cmd = "sudo " + self.cmd # "-runas" requires root privs #TODO: check for root?
-            self.cmd += " -runas %s " % runas
+      if runas:
+        self.cmd = "sudo " + self.cmd # "-runas" requires root privs #TODO: check for root?
+        self.cmd += " -runas %s " % runas
 
-
-    #TODO: rename to is_running()
-    def is_started(self):
+    def is_running(self):
         try:
-            pid = int(open(self.pidfile).readline().strip())
+          pid = int(open(self.pidfile).readline().strip())
         except IOError as err:
-            if err.errno == errno.EACCES:
-                raise StatusUnknown("cannot read peadfile:", err)
-            elif err.errno == errno.ENOENT:
-                return False
-            raise
+          if err.errno == errno.EACCES:
+            raise StatusUnknown("cannot read peadfile:", err)
+          elif err.errno == errno.ENOENT:
+            return False
+          raise
 
         try:
-            os.kill(pid, 0)
-            return pid
+          os.kill(pid, 0)
+          return pid
         except OSError as err:
-            if err.errno == errno.ESRCH:
-                os.unlink(self.pidfile)
-                return False
-            raise
-
+          if err.errno == errno.ESRCH:
+            os.unlink(self.pidfile)
+            return False
+          raise
 
     def start(self):
-        if self.is_started():
-            print("Instance is allready started!")
-            return False
-        #print(self.cmd) ; return #TODO
+      if self.is_running():
+        print("Instance is allready started!")
+        return False
+      #print(self.cmd) ; return #TODO
 
-        print("spawning", self.cmd)
-        self.tmux.run(self.cmd, name=self.name)
-
+      print("spawning", self.cmd)
+      self.tmux.run(self.cmd, name=self.name)
 
     #TODO: check its uniqueness
     def gen_mac(self):
-        #from http://mediakey.dk/~cc/generate-random-mac-address-for-e-g-xen-guests/
-        mac = [ 0x52, 0x54, 0x00,
-        random.randint(0x00, 0xff),
-        random.randint(0x00, 0xff),
-        random.randint(0x00, 0xff) ]
-        return ':'.join(map(lambda x: "%02x" % x, mac))
-
+      #from http://mediakey.dk/~cc/generate-random-mac-address-for-e-g-xen-guests/
+      mac = [ 0x52, 0x54, 0x00,
+      random.randint(0x00, 0xff),
+      random.randint(0x00, 0xff),
+      random.randint(0x00, 0xff) ]
+      return ':'.join(map(lambda x: "%02x" % x, mac))
 
     def kill(self):
-        pid = self.is_started()
-        if not pid:
-            return
-        os.kill(pid, signal.SIGTERM)
-
+      pid = self.is_running()
+      if not pid:
+        return
+      os.kill(pid, signal.SIGTERM)
 
     def send_cmd(self, cmd):
-        if isinstance(cmd, str):
-            cmd = cmd.encode()
-        if not cmd.endswith(b'\n'):
-            cmd += b'\n'
+      if isinstance(cmd, str):
+        cmd = cmd.encode()
+      if not cmd.endswith(b'\n'):
+        cmd += b'\n'
 
-        answer = b""
-        try:
-            s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-            s.settimeout(1)
-            log.debug("connecting to %s" % self.monfile)
-            s.connect(self.monfile)
-            s.send(cmd)
-            answer = s.recv(BUF_SIZE)
-            if len(answer) == BUF_SIZE:
-                log.error("too long answer, truncating")
-            log.notice(answer.decode(errors='replace'))
-        except:
-            raise
+      answer = b""
+      try:
+        s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        s.settimeout(1)
+        log.debug("connecting to %s" % self.monfile)
+        s.connect(self.monfile)
+        s.send(cmd)
+        answer = s.recv(BUF_SIZE)
+        if len(answer) == BUF_SIZE:
+          log.error("too long answer, truncating")
+        log.notice(answer.decode(errors='replace'))
+      except:
+        raise
 
-        return answer
-
+      return answer
 
     def reboot(self):
-        self.send_cmd("sendkey ctrl-alt-delete")
-
+      self.send_cmd("sendkey ctrl-alt-delete")
 
     def shutdown(self):
-        self.send_cmd("system_powerdown")
-
+      self.send_cmd("system_powerdown")
 
     def reset(self):
-        self.send_cmd("system_reset")
-
+      self.send_cmd("system_reset")
 
     def print_status(self):
-        print("{name}".format(name=self.name))
-        try:
-            pid = self.is_started()
-            status = "UP (pid %s)" % pid if pid else "DOWN"
-        except StatusUnknown as err:
-            status = "UNKNOWN (%s)" % err
-        print ("  status:", status)
-
+      print("{name}".format(name=self.name))
+      try:
+        pid = self.is_running()
+        status = "UP (pid %s)" % pid if pid else "DOWN"
+      except StatusUnknown as err:
+        status = "UNKNOWN (%s)" % err
+      print ("  status:", status)
 
     def __repr__(self):
-        return "<KVM(name=\"{name}\")>".format(name=self.name)
+      return "<KVM(name=\"{name}\")>".format(name=self.name)
 
 
 
@@ -166,12 +154,10 @@ class CMD(CLI):
     def __init__(self, instances):
         self.instances = instances
 
-
     @command("list")
     def do_list(self):
         for kvm in self.instances:
             print(kvm)
-
 
     @command("[name] start")
     @command("start [name]")
@@ -181,7 +167,6 @@ class CMD(CLI):
         print("Starting %s" % name)
         self.instances[name].start()
 
-
     @command("start all")
     def do_start_all(self):
         log.debug("starting all not started instances")
@@ -190,65 +175,56 @@ class CMD(CLI):
             if sleep:
                 log.debug("sleeping for %s before launchin another instance" % sleep)
             time.sleep(sleep)
-            if instance.is_started():
+            if instance.is_running():
                 log.debug("skipping %s because it is allready started" % instance)
                 continue
             log.info("Starting %s" % instance)
             instance.start()
             sleep = instance.sleep
 
-
     @command("console")
     @command("console [name]")
     @command("[name] console")
     def do_console(self, name=None):
         print("attaching", name)
-        if name and not instances[name].is_started():
+        if name and not instances[name].is_running():
                 sys.exit("Instance is not started")
         tmux.attach(name=name)
-
 
     @command("status")
     def do_status(self):
         for instance in self.instances.values():
             instance.print_status()
 
-
     @command("kill all") #TODO: doesnt work
     def do_kill_all(self):
         for instance in self.instances.values():
             instance.kill()
-
 
     @command("[name] shutdown")
     @command("shutdown [name]")
     def do_shutdown(self, name=None):
         self.instances[name].shutdown()
 
-
     @command("[name] kill")
     @command("kill [name]")
     def do_kill(self, name=None):
         instances[name].kill()
-
 
     @command("[name] reboot")
     @command("reboot [name]")
     def do_reboot(self, name=None):
         instances[name].reboot()
 
-
     @command("[name] reset")
     @command("reset [name]")
     def do_reset(self, name=None):
         instances[name].reset()
 
-
     @command("version")
     def do_print_version(self):
         print("version: ", __version__)
         print(sys.version)
-
 
     def do_default(self):
         return self.do_status()
