f60938af2f87e10a7a81a80af23b9180c9ef8314
Alexandre Kandalintsev <spam@messir.net>
2013-03-29 13:50:41 +0100

    setup.py: fixed after lib rename

--- setup.py
+++ setup.py
@@ -2,13 +2,11 @@
 
 from distutils.core import setup
 from kvmc import __version__
-setup(name='kvmc',
+setup(name='libvmc',
       version=str(__version__),
       author="Kandalintsev Alexandre",
       author_email='spam@messir.net',
       license="GPLv3",
       description="KVM Commander, a tool to manage your virtual machines",
-      py_modules=["kvmc"],
-      #packages=['useful'],
-      #package_dir = {'useful': './'}
+      py_modules=["libvmc"],
 )
